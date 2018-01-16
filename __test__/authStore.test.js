28d0b4732e6bbbd310c85dcb300e84c2fb24b33f
simonxu <xugaofan@foxmail.com>
2018-01-16 11:02:20 +0800

    support promise test

--- __test__/authStore.test.js
+++ __test__/authStore.test.js
@@ -4,12 +4,21 @@ import store from '../assets/stores/authStore'
 jest.useFakeTimers()
 
 describe('authStore', () => {
-  it("should set username and password", () => {
+  it('should set username and password', () => {
     store.setUsername('username')
     expect(store.username).toBe('username')
     store.setPassword('password')
     expect(store.password).toBe('password')
   })
 
-  // TODO test promise
+  it('should toggle inProgress when login', () => {
+    jest.advanceTimersByTime(2000)
+    expect(store.inProgress).toBe(false)
+    let promise = store.login()
+    expect(store.inProgress).toBe(true)
+    jest.advanceTimersByTime(2000)
+    promise.then(() => {
+      expect(store.inProgress).toBe(false)
+    })
+  })
 })
